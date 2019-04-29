d270d75c9eda628488da27b4ecf3d652573f7095
Ayah osama <aya.osama@smart-medicalservices.com>
2019-04-30 00:09:37 +0200

    edit

--- author/views.py
+++ author/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticated
 
 from author import serializers
 from author.models import Author
@@ -9,5 +10,5 @@ from author.models import Author
 class AuthorViewset(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = serializers.AuthorSerializer
-    permission_classes = permissions.IsAuthenticatedOrReadOnly
+    permission_classes = IsAuthenticated
 
