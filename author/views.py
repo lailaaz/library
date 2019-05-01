03e950ee38113df2fe141618df7084a4c40461b9
Ayah osama <aya.osama@smart-medicalservices.com>
2019-05-01 20:24:31 +0200

    edit permissions

--- author/views.py
+++ author/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticated
 
 from author import serializers
 from author.models import Author, Category, Book, Branch, BranchBookStock, Bill, Invoice
@@ -9,35 +10,35 @@ from author.models import Author, Category, Book, Branch, BranchBookStock, Bill,
 class AuthorViewset(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = serializers.AuthorSerializer
-    # permission_classes = permissions.IsAuthenticated
+    permission_classes = [IsAuthenticated, ]
 
 class CategoryViewset(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = serializers.CategorySerializer
-    # permission_classes = permissions.IsAuthenticated
+    permission_classes = [IsAuthenticated, ]
 
 class BookViewset(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = serializers.BookSerializer
-    # permission_classes = permissions.IsAuthenticated
+    permission_classes = [IsAuthenticated, ]
 
 class BranchViewset(viewsets.ModelViewSet):
     queryset = Branch.objects.all()
     serializer_class = serializers.BranchSerializer
-    # permission_classes = permissions.IsAuthenticated
+    permission_classes = [IsAuthenticated, ]
 
 class BranchBookStockViewset(viewsets.ModelViewSet):
     queryset = BranchBookStock.objects.all()
     serializer_class = serializers.BranchBookStockSerializer
-    # permission_classes = permissions.IsAuthenticated
+    permission_classes = [IsAuthenticated, ]
 
 class BillViewset(viewsets.ModelViewSet):
     queryset = Bill.objects.all()
     serializer_class = serializers.BillSerializer
-    # permission_classes = permissions.IsAuthenticated
+    permission_classes = [IsAuthenticated, ]
 
 class InvoiceViewset(viewsets.ModelViewSet):
     queryset = Invoice.objects.all()
     serializer_class = serializers.InvoiceSerializer
-    # permission_classes = permissions.IsAuthenticated
+    permission_classes = [IsAuthenticated, ]
 
